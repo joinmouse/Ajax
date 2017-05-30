3c59589e054d33154e0da662f16c8e42e9db865e
wuqi <908380434@qq.com>
2017-05-30 12:27:50 +0800

    add

---
+++ load-more/原生JS实现/.router.js
@@ -0,0 +1,20 @@
+function setRouter(app){ 
+ var router = app; 
+
+//服务端 router.js
+
+app.get('/loadMore', function(req, res) {
+
+	var idx = req.query.idx
+	var len = req.query.len
+	var data = []
+
+	for(var i = 0; i < len; i++) {
+		data.push('内容' + (parseInt(idx) + i))
+	}
+
+	res.send(data);
+});
+
+}
+ module.exports.setRouter = setRouter
